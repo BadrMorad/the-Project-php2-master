7b1f2fc03291799723dbb21ee04e2e49189dda85
BadrMorad <moradpro989@gmail.com>
2024-10-21 11:24:50 +0200

    php

--- exo10.php
+++ exo10.php
@@ -1,5 +1,25 @@
-<h1>Exercice 9</h1>
+<h1>Exercice 10</h1>
 
 <h2>Résultat</h2>
 
 <?php
+
+
+$nomsInput = ["Nom", "Prénom", "Adresse e-mail", "Ville"];
+
+function afficherFormulaire($nomsInput){
+
+    $result = "<form>";
+
+    foreach ($nomsInput as $nom) {
+
+        $result .= "<label>". $nom ." : <br></label><input type=text><br>";
+    }
+
+    $result .= "</form>";
+
+    return $result;
+}
+
+echo afficherFormulaire($nomsInput);
+?>
