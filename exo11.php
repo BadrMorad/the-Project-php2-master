03d092ee66436c155e25f40c831c5e19c228ccce
BadrMorad <moradpro989@gmail.com>
2024-10-21 16:59:17 +0200

    php

--- exo11.php
+++ exo11.php
@@ -5,12 +5,16 @@
 <?php
 
 function formaterDateFr($date_string) {
+    $jours = ["dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"];
     $mois = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"];
+ 
     $timestamp = strtotime($date_string);
+    $jour_semaine = $jours[date("w", $timestamp)];
     $jour = date("d", $timestamp);
     $mois_nom = $mois[date("n", $timestamp) - 1];
     $annee = date("Y", $timestamp);
-    echo "$jour $mois_nom $annee";
+ 
+    echo "$jour_semaine $jour $mois_nom $annee";
 }
  
 formaterDateFr("2018-02-23");
